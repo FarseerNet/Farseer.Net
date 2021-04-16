b24f98bed8157f8086bac61767e485e707e6b6ab
niao <qyniao888@es3.com>
2021-04-16 21:29:39 +0800

    修改redis配置为appsettings.json

--- Framework/Farseer.Net.Cache.Redis/RedisModule.cs
+++ Framework/Farseer.Net.Cache.Redis/RedisModule.cs
@@ -18,19 +18,6 @@ namespace FS.Cache.Redis
         /// </summary>
         public override void PreInitialize()
         {
-            // 如果Redis配置没有创建，则创建它
-            var configResolver = IocManager.Resolve<IConfigResolver>();
-            InitConfig(configResolver);
-        }
-
-        private void InitConfig(IConfigResolver configResolver)
-        {
-            var config = configResolver.RedisConfig();
-            if (config == null || config.Items.Count == 0)
-            {
-                configResolver.Set(new RedisConfig { Items = new List<RedisItemConfig> { new RedisItemConfig { Name = "testRedis", Server = "10.9.15.32:6388,10.9.15.38:6379,10.9.15.33:6383,10.9.15.36:6389,10.9.15.34:6382,10.9.15.35:6382,syncTimeout=60000" } } });
-                configResolver.Save();
-            }
         }
 
         /// <summary>
