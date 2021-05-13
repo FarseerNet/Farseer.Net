ed769cb52ae75f407fed900ebc1065eb82eba9ab
steden <1470804@qq.com>
2021-05-13 14:30:29 +0800

    更新redis stream使用文档

--- Demo/Farseer.Net.MQ.RedisStreamDemo/Program.cs
+++ Demo/Farseer.Net.MQ.RedisStreamDemo/Program.cs
@@ -18,28 +18,27 @@ namespace Farseer.Net.MQ.RedisStreamDemo
 
             // ******************** 以下演示消息发送 *********************
             var redisStreamProduct = IocManager.Instance.Resolve<IRedisStreamProduct>("test");
-            redisStreamProduct.Send(DateTime.Now);
-            redisStreamProduct.Send(DateTime.Now);
-            redisStreamProduct.Send(DateTime.Now);
+            redisStreamProduct.Send(DateTime.Now).ToString();
+
+            // ******************** 以下为了准备测试1S内，能发多少条消息 *********************
             var startNew = Stopwatch.StartNew();
             startNew.Start();
             Thread.Sleep(1000);
             startNew.Stop();
-            // 以上也是JIT
-            
+            // 以上为了JIT，以防测试差异较大
+
             // ******************** 测试1秒内，能发送多少条消息 *********************
             var count = 0;
             startNew.Reset();
             startNew.Restart();
-            while (startNew.ElapsedMilliseconds<1000)
+            while (startNew.ElapsedMilliseconds < 1000)
             {
                 redisStreamProduct.Send(DateTime.Now.ToString());
-                //Thread.Sleep(10);
                 count++;
             }
 
             Console.WriteLine(count);
-            
+
             Thread.Sleep(-1);
         }
     }
