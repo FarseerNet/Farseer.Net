7f24b4b12da5572e25c8cf03ea163b5d3d278a63
steden <1470804@qq.com>
2021-06-29 20:54:08 +0800

    判断系统运行时环境改由原生的ASPNETCORE_ENVIRONMENT

--- Framework/Farseer.Net/Env.cs
+++ Framework/Farseer.Net/Env.cs
@@ -7,21 +7,21 @@ namespace FS
         /// <summary>
         ///     生产环境
         /// </summary>
-        public static bool IsPro => string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("FS_ENV"));
+        public static bool IsPro => string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"));
 
         /// <summary>
         ///     测试环境
         /// </summary>
-        public static bool IsTest => Environment.GetEnvironmentVariable("FS_ENV") == "test";
+        public static bool IsTest => Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "test";
 
         /// <summary>
         ///     本地环境
         /// </summary>
-        public static bool IsDev => Environment.GetEnvironmentVariable("FS_ENV") == "dev";
+        public static bool IsDev => Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "dev";
 
         /// <summary>
         ///     预发布环境
         /// </summary>
-        public static bool IsPre => Environment.GetEnvironmentVariable("FS_ENV") == "pre";
+        public static bool IsPre => Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "pre";
     }
 }
