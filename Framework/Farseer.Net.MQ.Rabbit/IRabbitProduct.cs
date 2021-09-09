9626ace3e15a9a6b32d1fdcba9effe837e0f8c95
steden <1470804@qq.com>
2021-09-09 16:45:56 +0800

    升级组件

--- Framework/Farseer.Net.MQ.Rabbit/IRabbitProduct.cs
+++ Framework/Farseer.Net.MQ.Rabbit/IRabbitProduct.cs
@@ -7,7 +7,7 @@ namespace FS.MQ.Rabbit
     public interface IRabbitProduct
     {
         /// <summary>
-        ///     发送消息（Routingkey默认配置中的RoutingKey；ExchangeName默认配置中的ExchangeName）
+        ///     发送消息（routingKey默认配置中的RoutingKey；ExchangeName默认配置中的ExchangeName）
         /// </summary>
         /// <param name="message">消息主体</param>
         /// <param name="funcBasicProperties">属性</param>
@@ -32,14 +32,14 @@ namespace FS.MQ.Rabbit
         bool Send(IEnumerable<string> message, string routingKey, string exchange, Action<IBasicProperties> funcBasicProperties = null);
 
         /// <summary>
-        ///     发送消息（Routingkey默认配置中的RoutingKey；ExchangeName默认配置中的ExchangeName）
+        ///     发送消息（routingKey默认配置中的RoutingKey；ExchangeName默认配置中的ExchangeName）
         /// </summary>
         /// <param name="message">消息主体</param>
         /// <param name="funcBasicProperties">属性</param>
         bool Send(IEnumerable<string> message, Action<IBasicProperties> funcBasicProperties = null);
 
         /// <summary>
-        ///     发送消息（Routingkey默认配置中的RoutingKey；ExchangeName默认配置中的ExchangeName）
+        ///     发送消息（routingKey默认配置中的RoutingKey；ExchangeName默认配置中的ExchangeName）
         /// </summary>
         /// <param name="message">消息主体</param>
         /// <param name="routingKey">路由KEY名称</param>
@@ -47,7 +47,7 @@ namespace FS.MQ.Rabbit
         bool Send(string message, string routingKey, Action<IBasicProperties> funcBasicProperties = null);
 
         /// <summary>
-        ///     发送消息（Routingkey默认配置中的RoutingKey；ExchangeName默认配置中的ExchangeName）
+        ///     发送消息（routingKey默认配置中的RoutingKey；ExchangeName默认配置中的ExchangeName）
         /// </summary>
         /// <param name="message">消息主体</param>
         /// <param name="routingKey">路由KEY名称</param>
