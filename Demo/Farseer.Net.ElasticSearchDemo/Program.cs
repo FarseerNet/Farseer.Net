3188806c803954a33ea0722667f20a0e73b6fafe
steden <1470804@qq.com>
2021-11-22 22:25:30 +0800

    添加组合demo

--- Demo/Farseer.Net.ElasticSearchDemo/Program.cs
+++ Demo/Farseer.Net.ElasticSearchDemo/Program.cs
@@ -18,14 +18,14 @@ namespace Farseer.Net.ElasticSearchDemo
                 Desc     = $"大家好，我是steden,我今年{DateTime.Now.Second}岁"
             });
 
-            TestContext.Data.User.Where(o => o.UserName.Contains("ste")).ToList();
+            TestContext.Data.User.Where(q => q.Term(t => t.Age, 33)).Where(o => o.UserName.Contains("ste")).ToList();
             TestContext.Data.User.Where(o => o.Desc.Contains("我今年")).ToList();
             TestContext.Data.User.Where(o => o.Desc.StartsWith("大家好")).ToList();
             TestContext.Data.User.Where(o => o.UserName.EndsWith("en")).ToList();
-            //var lst1 = TestContext.Data.User.Where(o => o.UserName != "aaa").ToList();
-            // var lst2 = TestContext.Data.User.Where(o => o.Age      == 30).Where(o => o.UserName == "aaaa").ToList();
-            // var lst3 = TestContext.Data.User.Where(o => o.UserName == "steden" && o.Age == 18).ToList();
-            // var lst4 = TestContext.Data.User.Where(o => o.UserName == "steden" || o.Age >= 10).ToList();
+            TestContext.Data.User.Where(o => o.UserName != "aaa").ToList();
+            TestContext.Data.User.Where(o => o.Age      == 30).Where(o => o.UserName == "aaaa").ToList();
+            TestContext.Data.User.Where(o => o.UserName == "steden" && o.Age == 18).ToList();
+            TestContext.Data.User.Where(o => o.UserName == "steden" || o.Age >= 10).ToList();
         }
     }
 }
