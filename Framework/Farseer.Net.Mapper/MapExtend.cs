71d79769c2fedb4053d9b1488c612589683ea606
steden <1470804@qq.com>
2021-12-12 15:57:45 +0800

    判断是否为null前需要先await

--- Framework/Farseer.Net.Mapper/MapExtend.cs
+++ Framework/Farseer.Net.Mapper/MapExtend.cs
@@ -8,8 +8,12 @@ namespace FS.Extends
 {
     public static class MapExtend
     {
-        public static       List<TDestination>       Map<TDestination>(this               IEnumerable         source) => source == null ? null : MapperHelper.Mapper.Map<List<TDestination>>(source: source);
-        public static async Task<List<TDestination>> MapAsync<TDestination, TSource>(this Task<List<TSource>> source) => source == null ? default : MapperHelper.Mapper.Map<List<TDestination>>(source: await source);
+        public static List<TDestination> Map<TDestination>(this IEnumerable source) => source == null ? null : MapperHelper.Mapper.Map<List<TDestination>>(source: source);
+        public static async Task<List<TDestination>> MapAsync<TDestination, TSource>(this Task<List<TSource>> sourceTask)
+        {
+            var source = await sourceTask;
+            return source == null ? default : MapperHelper.Mapper.Map<List<TDestination>>(source);
+        }
 
         public static       TDestination       Map<TDestination>(this               object        source) => source == null ? default : MapperHelper.Mapper.Map<TDestination>(source: source);
         public static async Task<TDestination> MapAsync<TDestination, TSource>(this Task<TSource> source) => source == null ? default : MapperHelper.Mapper.Map<TDestination>(source: await source);
