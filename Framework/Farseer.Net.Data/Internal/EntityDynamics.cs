709f5d96873a701e00876d73983c74f66bde0d49
steden <1470804@qq.com>
2021-12-21 17:09:53 +0800

    支持JSON自动序列化、反序列化到数据库与实体

--- Framework/Farseer.Net.Data/Internal/EntityDynamics.cs
+++ Framework/Farseer.Net.Data/Internal/EntityDynamics.cs
@@ -3,22 +3,24 @@
 // 时间：2016-08-22 10:20
 // ********************************************
 
-#if !CORE
-#else
 using Microsoft.CodeAnalysis.CSharp.Scripting;
 using Microsoft.CodeAnalysis.Scripting;
-#endif
 using System;
+using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using FS.Core;
+using FS.Core.Mapping.Attribute;
 using FS.Data.Cache;
 using FS.Data.Infrastructure;
 using FS.Data.Map;
 using FS.Extends;
 using FS.Utils.Common;
+using Newtonsoft.Json;
 
 namespace FS.Data.Internal
 {
@@ -38,6 +40,8 @@ namespace FS.Data.Internal
 
             lock (objLock)
             {
+                if (Cache.ContainsKey(key: entityType)) return Cache[key: entityType];
+
                 var newType = BuildEntity(entityType: entityType);
                 Cache.TryAdd(key: entityType, value: newType);
                 return newType;
@@ -49,19 +53,10 @@ namespace FS.Data.Internal
         /// </summary>
         private Type BuildEntity(Type entityType)
         {
-            //var entityType = typeof(TEntity);
             var setPhysicsMap = SetMapCacheManger.Cache(key: entityType); // new SetPhysicsMap(entityType); 
             var clsName       = entityType.Name + "ByDataRow";            // 类名
 
             var sb = new StringBuilder();
-#if !CORE
-            sb.AppendLine("using System;");
-            sb.AppendLine("using System.Collections.Generic;");
-            sb.AppendLine("using FS.Core;");
-            sb.AppendLine("using FS.Utils.Common;");
-            sb.AppendLine("using FS.Data.Infrastructure;");
-            sb.AppendLine($"namespace {entityType.Namespace}\r\n{{");
-#endif
             sb.AppendLine(value: $"public class {clsName} : {entityType.FullName}\r\n{{");
             // DataRow构造
             sb.AppendLine(value: CreateToList(entityType: entityType));
@@ -69,77 +64,14 @@ namespace FS.Data.Internal
             sb.AppendLine(value: CreateToEntity(entityType: entityType, setPhysicsMap: setPhysicsMap));
 
             sb.AppendLine(value: "}");
-#if !CORE
-            sb.AppendLine("}");
-#endif
 
-#if !CORE
-            CompilerResults results = null;
-            try
-            {
-                var compilerParams = new CompilerParameters
-                {
-                    CompilerOptions = "/target:library /optimize",   //编译器选项设置
-                    GenerateInMemory = true,                        //编译时在内存输出
-                    IncludeDebugInformation = false                 //生成调试信息
-                };
-
-                //添加相关的引用
-                compilerParams.ReferencedAssemblies.Add("System.dll");
-                compilerParams.ReferencedAssemblies.Add("System.Data.dll");
-                compilerParams.ReferencedAssemblies.Add("System.Xml.dll");
-                var ass = AppDomain.CurrentDomain.GetAssemblies();  //  得到当前所有程序集
-                compilerParams.ReferencedAssemblies.Add(ass.FirstOrDefault(o => o.ManifestModule.Name == "Farseer.Net.dll")?.Location);
-                compilerParams.ReferencedAssemblies.Add(ass.FirstOrDefault(o => o.ManifestModule.Name == "Farseer.Net.Core.dll")?.Location);
-                compilerParams.ReferencedAssemblies.Add(ass.FirstOrDefault(o => o.ManifestModule.Name == "Farseer.Net.Data.dll")?.Location);
-
-
-                // 加载成员依赖的类型所在的程序集
-                var properties = entityType.GetProperties();
-                foreach (var propertyInfo in properties)
-                {
-                    // 找到真实程序集
-                    var declaringType = propertyInfo.PropertyType.GetNullableArguments();
-                    while (declaringType.IsGenericType) { declaringType = declaringType.GetGenericType(); }
-
-                    compilerParams.ReferencedAssemblies.Add(declaringType.Assembly.Location);
-                }
-
-                // 需要把基类型的dll，也载进来
-                var baseType = entityType;
-                while (baseType != null)
-                {
-                    compilerParams.ReferencedAssemblies.Add(baseType.Assembly.Location);
-                    baseType = baseType.BaseType;
-                }
-
-                var compiler = CodeDomProvider.CreateProvider("CSharp");
-                //编译
-                results = compiler.CompileAssemblyFromSource(compilerParams, sb.ToString());
-                return results.CompiledAssembly.GetExportedTypes()[0];
-            }
-            catch (Exception exp)
-            {
-                if (results != null)
-                {
-                    var error = new string[results.Errors.Count];
-                    for (int i = 0; i < error.Length; i++)
-                    {
-                        error[i] = results.Errors[i].ErrorText;
-                        IocManager.Instance.Logger.Error(error[i]);
-                    }
-                    throw new Exception(error.ToString(","));
-                }
-                IocManager.Instance.Logger.Error(exp.ToString());
-                throw;
-            }
-#else
-            var scriptOptions = ScriptOptions.Default;
-            scriptOptions = scriptOptions.AddReferences(typeof(List<>).Assembly, typeof(ConvertHelper).Assembly, typeof(MapingData).Assembly, entityType.Assembly);
-            scriptOptions = scriptOptions.AddImports("System.Collections.Generic");
-            scriptOptions = scriptOptions.AddImports("FS.Core");
-            scriptOptions = scriptOptions.AddImports("FS.Utils.Common");
-            scriptOptions = scriptOptions.AddImports("FS.Data.Infrastructure");
+            var scriptOptions = ScriptOptions.Default
+                                             .AddReferences(typeof(List<>).Assembly, typeof(ConvertHelper).Assembly, typeof(MapingData).Assembly, entityType.Assembly)
+                                             .AddImports("System.Collections.Generic")
+                                             .AddImports("Newtonsoft.Json")
+                                             .AddImports("FS.Core")
+                                             .AddImports("FS.Utils.Common")
+                                             .AddImports("FS.Data.Infrastructure");
 
             // 加载成员依赖的类型所在的程序集
             var properties = entityType.GetProperties();
@@ -160,11 +92,12 @@ namespace FS.Data.Internal
                 baseType      = baseType.BaseType;
             }
 
-            var scriptState = CSharpScript.Create(code: sb.ToString(), options: scriptOptions).RunAsync().Result;
+            var runTask  = CSharpScript.Create(code: sb.ToString(), options: scriptOptions).RunAsync();
+            Task.WaitAll(runTask);
+            var scriptState = runTask.Result;
             var asmName     = scriptState.Script.GetCompilation().AssemblyName;
             var assembly    = AppDomain.CurrentDomain.GetAssemblies().First(predicate: a => a.FullName.StartsWith(value: asmName, comparisonType: StringComparison.OrdinalIgnoreCase));
             return assembly.DefinedTypes.FirstOrDefault(predicate: o => o.Name == clsName);
-#endif
         }
 
         /// <summary> 生成ToList转换方法 </summary>
@@ -210,6 +143,7 @@ namespace FS.Data.Internal
             var sb = new StringBuilder();
             foreach (var map in setPhysicsMap.MapList)
             {
+                if (map.Value.Field.StorageType == EumStorageType.Ignore) continue;
                 // 字段名
                 var filedName = map.Value.Field.IsFun ? map.Key.Name : map.Value.Field.Name;
                 // 类型转换
@@ -220,33 +154,60 @@ namespace FS.Data.Internal
                 // case 字段名
                 sb.Append(value: $"\t\t\tcase \"{filedName.ToUpper()}\":\r\n\t\t\t\t");
                 // 使用FS的ConvertHelper 进行类型转换泛型类型
-                if (propertyType.IsGenericType)
+                switch (map.Value.Field.StorageType)
                 {
-                    if (propertyType.IsArray) // 数组类型
+                    case EumStorageType.Direct:
                     {
-                        var asType = $"{propertyType.GetGenericArguments()[0].FullName}[]";
-                        sb.Append(value: $"{propertyAssign} = ConvertHelper.ConvertType(col,typeof({asType})) as {asType}; ");
+                        // 字符串不需要处理
+                        if (propertyType == typeof(string))
+                            sb.Append(value: $"{propertyAssign} = col.ToString();");
+                        else if (propertyType.IsEnum)
+                            sb.Append(value: $"if (typeof({propertyType.FullName}).GetEnumUnderlyingType() == col.GetType()) {{ {propertyAssign} = ({propertyType.FullName})col; }} else {{ if (System.Enum.TryParse(col.ToString(), out {propertyType.FullName} {filedName}_Out)) {{ {propertyAssign} = {filedName}_Out; }} }}");
+                        else if (propertyType == typeof(bool))
+                            sb.Append(value: $"{propertyAssign} = ConvertHelper.ConvertType(col,false);");
+                        else if (!propertyType.IsClass)
+                        {
+                            sb.Append(value: $"if (col is {propertyType.FullName}) {{ {propertyAssign} = ({propertyType.FullName})col; }} else {{ if ({propertyType.FullName}.TryParse(col.ToString(), out {propertyType.FullName} {filedName}_Out)) {{ {propertyAssign} = {filedName}_Out; }} }}");
+                            // ClickHouse的DateTime类型，需要将UTC转为Local
+                            if (setPhysicsMap.InternalContext.ContextConnection.DbType == eumDbType.ClickHouse && propertyType == typeof(DateTime)) sb.Append(value: map.Key.PropertyType.IsGenericType ? $"{propertyAssign} = {propertyAssign}.GetValueOrDefault().ToLocalTime();" : $"{propertyAssign} = {propertyAssign}.ToLocalTime();");
+                        }
+                        break;
                     }
-                    else // List集合
-                        sb.Append(value: $"{propertyAssign} = StringHelper.ToList<{propertyType.GetGenericArguments()[0].FullName}>(col.ToString()); ");
-                }
-                else
-                {
-                    // 字符串不需要处理
-                    if (propertyType == typeof(string))
-                        sb.Append(value: $"{propertyAssign} = col.ToString();");
-                    else if (propertyType.IsEnum)
-                        sb.Append(value: $"if (typeof({propertyType.FullName}).GetEnumUnderlyingType() == col.GetType()) {{ {propertyAssign} = ({propertyType.FullName})col; }} else {{ {propertyType.FullName} {filedName}_Out; if (System.Enum.TryParse(col.ToString(), out {filedName}_Out)) {{ {propertyAssign} = {filedName}_Out; }} }}");
-                    else if (propertyType == typeof(bool))
-                        sb.Append(value: $"{propertyAssign} = ConvertHelper.ConvertType(col,false);");
-                    else if (!propertyType.IsClass)
+                    case EumStorageType.Json:
                     {
-                        sb.Append(value: $"if (col is {propertyType.FullName}) {{ {propertyAssign} = ({propertyType.FullName})col; }} else {{ {propertyType.FullName} {filedName}_Out; if ({propertyType.FullName}.TryParse(col.ToString(), out {filedName}_Out)) {{ {propertyAssign} = {filedName}_Out; }} }}");
-                        // ClickHouse的DateTime类型，需要将UTC转为Local
-                        if (setPhysicsMap.InternalContext.ContextConnection.DbType == eumDbType.ClickHouse && propertyType == typeof(DateTime)) sb.Append(value: map.Key.PropertyType.IsGenericType ? $"{propertyAssign} = {propertyAssign}.GetValueOrDefault().ToLocalTime();" : $"{propertyAssign} = {propertyAssign}.ToLocalTime();");
-                    }
-                }
+                        if (propertyType.IsGenericType)
+                        {
+                            string type;
+                            if (propertyType.IsArray) // 数组
+                            {
+                                type = $"{propertyType.GenericTypeArguments[0].FullName}[]";
+                            }
+                            // 字典
+                            else if (propertyType.GetInterfaces().Any(o => o == typeof(IDictionary)))
+                            {
+                                type = $"Dictionary<{string.Join(",", propertyType.GenericTypeArguments.Select(o => o.FullName))}>";
+                            }
+                            else
+                            {
+                                type = $"List<{propertyType.GenericTypeArguments[0].FullName}>";
+                            }
+                            sb.Append(value: $"{propertyAssign} = JsonConvert.DeserializeObject(col.ToString(),typeof({type})) as {type}; ");
+                        }
 
+                        break;
+                    }
+                    case EumStorageType.Array:
+                    {
+                        if (propertyType.IsArray) // 数组类型
+                        {
+                            var asType = $"{propertyType.GetGenericArguments()[0].FullName}[]";
+                            sb.Append(value: $"{propertyAssign} = ConvertHelper.ConvertType(col,typeof({asType})) as {asType}; ");
+                        }
+                        else // List集合
+                            sb.Append(value: $"{propertyAssign} = StringHelper.ToList<{propertyType.GetGenericArguments()[0].FullName}>(col.ToString()); ");
+                        break;
+                    }
+                }
                 // 退出case
                 sb.AppendLine(value: "break;");
             }
