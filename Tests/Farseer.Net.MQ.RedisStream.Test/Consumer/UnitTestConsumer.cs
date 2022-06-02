94d12bfe14816a3df1425b2774c11e236e347bd8
steden <1470804@qq.com>
2022-06-03 02:06:32 +0800

    新增RedisStream组件的单元测试

--- Tests/Farseer.Net.MQ.RedisStream.Test/Consumer/UnitTestConsumer.cs
+++ Tests/Farseer.Net.MQ.RedisStream.Test/Consumer/UnitTestConsumer.cs
@@ -1,30 +1,30 @@
 using System;
 using System.Threading.Tasks;
+using FS.Core.Abstract.RedisStream;
 using FS.Extends;
 using FS.MQ.RedisStream;
 using FS.MQ.RedisStream.Attr;
-using StackExchange.Redis;
 
 namespace Farseer.Net.MQ.RedisStream.Test.Consumer
 {
     /// <summary>
     ///     消费客户端
     /// </summary>
-    [Consumer(Enable = true, Server = "test2", GroupName = "", QueueName = "test2", PullCount = 2, ConsumeThreadNums = 1)]
+    [Consumer(Enable = true, Server = "UnitTest", GroupName = "", QueueName = "UnitTest", PullCount = 2, ConsumeThreadNums = 1)]
     public class UnitTestConsumer : IListenerMessage
     {
         public static long ID;
-        public Task<bool> Consumer(StreamEntry[] messages, ConsumeContext context)
+        public Task<bool> Consumer(ConsumeContext context)
         {
-            foreach (var message in messages)
+            foreach (var redisStreamMessage in context.RedisStreamMessages)
             {
-                ID = message.Values[0].ConvertType(0L);
-                context.Ack(message: message);
+                ID = redisStreamMessage.Message.ConvertType(0L);
+                redisStreamMessage.Ack();
             }
 
             return Task.FromResult(result: true);
         }
 
-        public Task<bool> FailureHandling(StreamEntry[] messages, ConsumeContext context) => throw new NotImplementedException();
+        public Task<bool> FailureHandling(ConsumeContext context) => throw new NotImplementedException();
     }
 }
