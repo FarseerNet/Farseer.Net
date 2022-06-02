9c8277556149fdc2fe258632d87378568c4f5ba0
steden <1470804@qq.com>
2022-06-02 22:33:23 +0800

    修改配置

--- Tests/Farseer.Net.Cache.Redis.Test/RedisCacheManager.cs
+++ Tests/Farseer.Net.Cache.Redis.Test/RedisCacheManager.cs
@@ -48,16 +48,18 @@ public class RedisCacheManager : BaseTests
 
         var redisItemConfigs = redisCacheManager.HashToList<RedisItemConfig>(key).ToPooledList();
         Assert.NotZero(redisItemConfigs.Count());
-        for (int i = 0; i < lst.Count; i++)
+        for (int i = 0; i < redisItemConfigs.Count; i++)
         {
-            Assert.AreEqual(lst[i].Name, redisItemConfigs[i].Name);
-            Assert.AreEqual(lst[i].Server, redisItemConfigs[i].Server);
+            var curItem = lst.FirstOrDefault(o => o.Name == redisItemConfigs[i].Name);
+            Assert.AreEqual(redisItemConfigs[i].Name, curItem.Name);
+            Assert.AreEqual(redisItemConfigs[i].Server, curItem.Server);
         }
 
         var redisItemConfig = redisCacheManager.HashToEntity<RedisItemConfig>(key, "3");
         Assert.NotNull(redisItemConfig);
-        
-        Assert.AreEqual(lst[0].Server, redisItemConfig.Server);
+
+        var toEntity = lst.FirstOrDefault(o => o.Name == redisItemConfig.Name);
+        Assert.AreEqual(toEntity.Server, redisItemConfig.Server);
         redisCacheManager.Db.KeyDelete(key);
     }
 }
