fed375dca132801249bd7a1cf17f12ddfd87fcd6
steden <1470804@qq.com>
2022-06-04 21:03:29 +0800

    添加：TrackAttribute，手动埋点链路追踪

--- Demo/Farseer.Net.LinkTrackDemo/Program.cs
+++ Demo/Farseer.Net.LinkTrackDemo/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using FS;
+using FS.Core.Attribute.Data;
 using FS.Core.LinkTrack;
 
 namespace Farseer.Net.LinkTrackDemo
@@ -10,12 +11,23 @@ namespace Farseer.Net.LinkTrackDemo
         private static void Main(string[] args)
         {
             FarseerApplication.Run<StartupModule>().Initialize();
-            using (var trackEnd = FsLinkTrack.Track(message: ""))
-            {
-                throw new Exception(message: "aaaaa");
-            }
+            new TrackDemo().Execute();
 
             Thread.Sleep(millisecondsTimeout: -1);
+        }
+    }
+
+    [Track]
+    public class TrackDemo
+    {
+        public TrackDemo()
+        {
+
+        }
+        
+        public void Execute()
+        {
+
         }
     }
 }
---
+++ Framework/Farseer.Net.Core/Attribute/Data/TrackAttribute.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Linq;
+using FS.Core.LinkTrack;
+using PostSharp.Aspects;
+using PostSharp.Extensibility;
+using PostSharp.Serialization;
+
+namespace FS.Core.Attribute.Data;
+
+/// <summary>
+/// 手动埋点链路追踪
+/// </summary>
+[PSerializable]
+[AttributeUsage(AttributeTargets.Method                                                                 | AttributeTargets.Class | AttributeTargets.Constructor, AllowMultiple = true)]
+[MulticastAttributeUsage(MulticastTargets.Method, TargetMemberAttributes = MulticastAttributes.Instance | MulticastAttributes.Static)]
+public class TrackAttribute : MethodInterceptionAspect
+{
+    public override void OnInvoke(MethodInterceptionArgs args)
+    {
+        var returnType = ((System.Reflection.MethodInfo)args.Method).ReturnType.Name;
+        var argsString = string.Join(", ", args.Arguments.Select(p => $"{p.GetType().FullName} = {p}"));
+
+        var callName = args.Method.DeclaringType != null ? $"{args.Method.DeclaringType.Name}.{args.Method.Name}" : $"{args.Method.Name}";
+        using (FsLinkTrack.Track($"{returnType} {callName}({argsString})"))
+        {
+            args.Proceed();
+        }
+    }
+}
